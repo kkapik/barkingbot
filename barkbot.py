399196f466cc2f42ea175c4a76df94b6c3e183ac
Thomas Poncet <58975437+thomponcet64@users.noreply.github.com>
2021-03-09 08:51:18 +0100

    Add files via upload

---
+++ barkbot.py
@@ -0,0 +1,66 @@
+#importation module
+import discord
+import time
+
+from discord.ext import commands
+
+#créer le bot
+bot = commands.Bot(command_prefix='$')
+
+#détecter quand bor est pret
+@bot.event
+async def on_ready():
+    print("bot prêt")
+    await bot.change_presence(status= discord.Status.idle, activity=discord.Game("Prêt à barker"))
+  
+
+
+#cmd bienvenue
+@bot.command()
+async def bark_on(ctx, member : discord.Member):
+    pseudo = member.mention
+    auteur = ctx.author.mention
+    await ctx.send(f"{auteur} m'a demandé de bark sur {pseudo} :dog: Ouaf :dog:")
+    print(f"{auteur} m'a demandé de bark sur {pseudo} :dog: Ouaf :dog:")
+    await member.send(f":dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog:")
+    time.sleep(0.5)
+    await member.send(f":dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog:")
+    time.sleep(0.5)
+    await member.send(f":dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog:")
+    time.sleep(0.5)
+    await member.send(f":dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog:")
+    time.sleep(0.5)
+    await member.send(f":dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog:")
+    time.sleep(0.5)
+    await member.send(f":dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog:")
+    time.sleep(0.5)
+    await member.send(f":dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog:")
+    time.sleep(0.5)
+    await member.send(f":dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog:")
+    time.sleep(0.5)
+    await member.send(f":dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog:")
+    time.sleep(0.5)
+    await member.send(f":dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog: \n:dog: Ouaf Ouaf Ouaf :dog:")
+
+
+
+#vérifier erreur
+@bark_on.error
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Il manque un argument")
+
+
+
+
+
+
+
+
+#donner le jeton pour connection
+jeton = "ODA3OTExNDAwNTMzMzkzNDQ5.YB-4IA.fv8hQZbCKp6Q2eQ3lv6P0c0_m_A"
+
+#connection serveur
+bot.run(jeton)
+
+
